0e1d024196e1e39f2d602ea43c9605a473ceffa7
MichalSymfonia <michal.janusz@symfonia.pl>
2024-11-08 13:30:00 +0100

    pare lini kodu

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -1,18 +1,35 @@
-﻿using System;
+﻿using Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-
+//using EFCoreInMemoryDbDemo;
 namespace Bank
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-            
+           
+            //var builder = WebApplication.CreateBuilder(args);
+            //builder.Services.AddScoped<User, AccountHistory>();
+            //builder.Services.AddControllers();
+            //builder.Services.AddEndpointsApiExplorer();
+            //builder.Services.AddSwaggerGen();
+            //var app = builder.Build();
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+            //app.UseHttpsRedirection();
+            //app.UseAuthorization();
+            //app.MapControllers();
+            //app.Run();
+
+
         }
     }
 }
